f70558d86dd95cb4105ac642017f6a169e7a658b
Aleksandr Chudov <chudov.aleksandr94@gmail.com>
2018-05-24 19:18:33 +0300

    Added stopwatch application

---
+++ StopWatch/Models/StopwatchModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace StopWatch.Models
+{
+    public class StopwatchModel
+    {
+        public TimeSpan Elapsed => _sw.Elapsed;
+        public bool IsRunning => _sw.IsRunning;
+        public bool IsStopped => !_sw.IsRunning && _sw.ElapsedTicks == 0;
+        public bool IsPaused => !(IsRunning || IsStopped);
+        public readonly ReadOnlyObservableCollection<TimeSpan> Laps;
+
+        private readonly Stopwatch _sw = new Stopwatch();
+        private readonly ObservableCollection<TimeSpan> _laps = new ObservableCollection<TimeSpan>();
+
+        public StopwatchModel()
+        {
+            Laps = new ReadOnlyObservableCollection<TimeSpan>(_laps);
+        }
+
+        public void Start()
+        {
+            if (IsStopped)
+            {
+                ResetLaps();
+            }
+            _sw.Start();
+        }
+
+        public void Lap() => _laps.Add(_sw.Elapsed);
+
+        public void Stop() => _sw.Stop();
+
+        public void Reset()
+        {
+            _sw.Reset();
+            ResetLaps();
+        }
+
+        public void ResetLaps() => _laps.Clear();
+    }
+}
---
+++ StopWatch/ViewModels/StopwatchVM.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Threading;
+using Prism.Commands;
+using Prism.Mvvm;
+using StopWatch.Models;
+
+namespace StopWatch.ViewModels
+{
+    public class StopwatchVM : BindableBase
+    {
+        public TimeSpan Elapsed => _model.Elapsed;
+        public ReadOnlyObservableCollection<TimeSpan> Laps => _model.Laps;
+
+        public DelegateCommand StartCommand { get; }
+        public DelegateCommand StopCommand { get; }
+        public DelegateCommand ResetCommand { get; }
+        public DelegateCommand LapCommand { get; }
+
+        private readonly StopwatchModel _model = new StopwatchModel();
+        private readonly DispatcherTimer _timer;
+
+        public StopwatchVM()
+        {
+            StartCommand = new DelegateCommand(StopwatchStart, () => !_model.IsRunning);
+            StopCommand = new DelegateCommand(StopwatchStop, () => _model.IsRunning);
+            ResetCommand = new DelegateCommand(StopwatchReset, () => _model.IsPaused);
+            LapCommand = new DelegateCommand(StopwatchLap, () => _model.IsRunning);
+
+            _timer = new DispatcherTimer()
+            {
+                Interval = TimeSpan.FromMilliseconds(10),
+            };
+            _timer.Tick += (sender, args) => RaisePropertyChanged(nameof(Elapsed));
+        }
+
+        private void StopwatchStart()
+        {
+            _model.Start();
+            _timer.Start();
+            StopwatchStateChanged();
+        }
+
+        private void StopwatchStop()
+        {
+            _model.Stop();
+            _timer.Stop();
+            StopwatchStateChanged();
+        }
+
+        private void StopwatchReset()
+        {
+            _model.Reset();
+            RaisePropertyChanged(nameof(Elapsed));
+            StopwatchStateChanged();
+        }
+
+        private void StopwatchLap()
+        {
+            _model.Lap();
+        }
+
+        private void StopwatchStateChanged()
+        {
+            StartCommand.RaiseCanExecuteChanged();
+            StopCommand.RaiseCanExecuteChanged();
+            ResetCommand.RaiseCanExecuteChanged();
+            LapCommand.RaiseCanExecuteChanged();
+        }
+    }
+}
